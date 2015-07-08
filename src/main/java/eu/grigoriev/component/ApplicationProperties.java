54f42df75845ea5bf53cde149951e7455b324965
Sergey Grigoriev <sergey@grigoriev.eu>
2015-07-09 00:14:23 +0200

    refactoring: to configuration with annotations

--- src/main/java/eu/grigoriev/component/ApplicationProperties.java
+++ src/main/java/eu/grigoriev/component/ApplicationProperties.java
@@ -5,47 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationProperties {
-    @Value("${" + PROPERTIES.MYSQL.JDBC_DRIVER_CLASS_NAME + "}")
-    private String mysqlJdbcDriverClassName;
-    @Value("${" + PROPERTIES.MYSQL.JDBC_URL + "}")
-    private String mysqlJdbcUrl;
-    @Value("${" + PROPERTIES.MYSQL.JDBC_USERNAME + "}")
-    private String mysqlJdbcUsername;
-    @Value("${" + PROPERTIES.MYSQL.JDBC_PASSWORD + "}")
-    private String mysqlJdbcPassword;
-
     @Value("${" + PROPERTIES.TEST_MODE + "}")
     private Boolean testMode;
-
-    public String getMysqlJdbcUrl() {
-        return mysqlJdbcUrl;
-    }
-
-    public String getMysqlJdbcUsername() {
-        return mysqlJdbcUsername;
-    }
-
-    public String getMysqlJdbcPassword() {
-        return mysqlJdbcPassword;
-    }
-
-    public String getMysqlJdbcDriverClassName() {
-        return mysqlJdbcDriverClassName;
-    }
 
     public Boolean isTestMode() {
         return testMode;
     }
 
     public static class PROPERTIES {
-
-        public static class MYSQL {
-            public static final String JDBC_DRIVER_CLASS_NAME = "mysql.jdbc.driverClassName";
-            public static final String JDBC_URL = "mysql.jdbc.url";
-            public static final String JDBC_USERNAME = "mysql.jdbc.username";
-            public static final String JDBC_PASSWORD = "mysql.jdbc.password";
-        }
-
         public static final String TEST_MODE = "test.mode";
     }
 }
---
+++ src/main/java/eu/grigoriev/config/PersistenceConfig.java
@@ -0,0 +1,64 @@
+package eu.grigoriev.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class PersistenceConfig {
+
+    Properties hibernateProperties() {
+        return new Properties() {
+            {
+                setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+                setProperty("hibernate.hbm2ddl.auto", "update");
+                setProperty("hibernate.show_sql", "false");
+            }
+        };
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(new String[]{"eu.grigoriev.persistence"});
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Autowired
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/bets");
+        dataSource.setUsername("bets");
+        dataSource.setPassword("bets");
+
+        return dataSource;
+    }
+}
