73a66ce3d9474841a90237b53187215df26046c2
Sergey Grigoriev <sergey@grigoriev.eu>
2015-07-08 15:14:09 +0200

    junit login tests

--- src/test/java/eu/grigoriev/ControllerTest.java
+++ src/test/java/eu/grigoriev/ControllerTest.java
@@ -4,12 +4,24 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -17,7 +29,10 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration("file:src/main/webapp/WEB-INF/application-context.xml")
+@ContextConfiguration(locations = {
+        "file:src/main/webapp/WEB-INF/application-context.xml",
+        "file:src/main/webapp/WEB-INF/spring-security.xml"
+})
 public class ControllerTest {
 
     private MockMvc mockMvc;
@@ -31,17 +46,48 @@ public class ControllerTest {
         this.mockMvc = webAppContextSetup(this.wac).build();
     }
 
-    @Test
-    public void testIndex() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("Index"));
+    private void login() {
+        UserDetails userDetails = wac.getBean(InMemoryUserDetailsManager.class).loadUserByUsername("username");
+        Authentication authenticationToken = new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+
+    private void invalidRole() {
+        UserDetails userDetails = wac.getBean(InMemoryUserDetailsManager.class).loadUserByUsername("username");
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_INVALID"));
+        Authentication authenticationToken = new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), authorities);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+
+    private void logout() {
+        SecurityContextHolder.clearContext();
     }
 
     @Test
-    public void testLogin() throws Exception {
+    public void testIndexDashboard() throws Exception {
+        login();
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index/dashboard"));
+        logout();
+    }
+
+    @Test
+    public void testInvalidRole() throws Exception {
+        invalidRole();
+        try {
+            mockMvc.perform(get("/"));
+        } catch (Throwable e) {
+            assertTrue(e.getCause() instanceof AccessDeniedException);
+        }
+        logout();
+    }
+
+    @Test
+    public void testAuthLogin() throws Exception {
         mockMvc.perform(get("/auth/login"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("AuthLogin"));
+                .andExpect(view().name("auth/login"));
     }
 }
