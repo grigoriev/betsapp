e4df98650aa38212e3775fa14254ba78ced2542b
Sergey Grigoriev <sergey@grigoriev.eu>
2015-07-09 10:50:19 +0200

    application config

---
+++ src/main/java/eu/grigoriev/config/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package eu.grigoriev.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@ComponentScan("eu.grigoriev.component")
+@PropertySource({"classpath:application.properties"})
+public class ApplicationConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
